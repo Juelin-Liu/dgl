94cdec2e9ff9f558f630a44f966faecdc1b77a39
Juelin Liu <juelin@jupiter.cs.umass.edu>
2024-01-19 12:25:47 -0500

    clean up

--- src/graph/transform/metis_partition_hetero.cc
+++ src/graph/transform/metis_partition_hetero.cc
@@ -36,6 +36,11 @@ IdArray MetisPartition(
   idx_t ncon = 1;  // # balacing constraints.
   idx_t *xadj = static_cast<idx_t *>(mat.indptr->data);
   idx_t *adjncy = static_cast<idx_t *>(mat.indices->data);
+  idx_t *ewgt = nullptr;
+  if (mat.data.NumElements() == mat.indices.NumElements()) {
+    ewgt = static_cast<idx_t *>(mat.data->data);
+    LOG(INFO) << "using edge weights";
+  }
   idx_t nparts = k;
   IdArray part_arr = aten::NewIdArray(nvtxs);
   idx_t objval = 0;
@@ -76,103 +81,6 @@ IdArray MetisPartition(
       vwgt,    // the weights of the vertices
       NULL,    // The size of the vertices for computing
       // the total communication volume
-      NULL,     // The weights of the edges
-      &nparts,  // The number of partitions.
-      NULL,     // the desired weight for each partition and constraint
-      NULL,     // the allowed load imbalance tolerance
-      options,  // the array of options
-      &objval,  // the edge-cut or the total communication volume of
-      // the partitioning solution
-      part);
-
-  if (obj_cut) {
-    LOG(INFO) << "Partition a graph with " << g->NumVertices(0) << " nodes and "
-              << g->NumEdges(0) << " edges into " << k << " parts and "
-              << "get " << objval << " edge cuts";
-  } else {
-    LOG(INFO) << "Partition a graph with " << g->NumVertices(0) << " nodes and "
-              << g->NumEdges(0) << " edges into " << k << " parts and "
-              << "the communication volume is " << objval;
-  }
-
-  switch (ret) {
-    case METIS_OK:
-      return part_arr;
-    case METIS_ERROR_INPUT:
-      LOG(FATAL) << "Error in Metis partitioning: input error";
-    case METIS_ERROR_MEMORY:
-      LOG(FATAL) << "Error in Metis partitioning: cannot allocate memory";
-    default:
-      LOG(FATAL) << "Error in Metis partitioning: other errors";
-  }
-  // return an array of 0 elements to indicate the error.
-  return aten::NullArray();
-}
-
-IdArray MetisPartition(
-    NDArray indptr, NDArray indices, int k, NDArray vwgt_arr, NDArray ewgt_arr, const std::string &mode,
-    bool obj_cut) {
-  // Mode can only be "k-way" or "recursive"
-  CHECK(mode == "k-way" || mode == "recursive")
-      << "mode can only be \"k-way\" or \"recursive\"";
-  // The index type of Metis needs to be compatible with DGL index type.
-  CHECK_EQ(sizeof(idx_t), sizeof(int64_t))
-      << "Metis only supports int64 graph for now";
-
-  idx_t nvtxs = indptr.NumElements() - 1;
-  idx_t e_num = indices.NumElements();
-  idx_t ncon = 1;  // # balacing constraints.
-  idx_t *xadj = static_cast<idx_t *>(indptr->data);
-  idx_t *adjncy = static_cast<idx_t *>(indices->data);
-  idx_t nparts = k;
-  idx_t objval = 0;
-
-  IdArray part_arr = aten::NewIdArray(nvtxs);
-  idx_t *part = static_cast<idx_t *>(part_arr->data);
-
-  int64_t vwgt_len = vwgt_arr->shape[0];
-  int64_t ewgt_len = ewgt_arr->shape[0];
-
-  CHECK_EQ(sizeof(idx_t), vwgt_arr->dtype.bits / 8)
-      << "The vertex weight array doesn't have right type";
-  CHECK_EQ(sizeof(idx_t), ewgt_arr->dtype.bits / 8)
-      << "The edge weight array doesn't have right type";
-
-  idx_t *vwgt = NULL;
-  if (vwgt_len > 0) {
-    ncon = vwgt_len / nvtxs;
-    vwgt = static_cast<idx_t *>(vwgt_arr->data);
-  }
-
-  idx_t *ewgt = NULL;
-  if (ewgt_len > 0) {
-    ewgt = static_cast<idx_t *>(ewgt_arr->data);
-  }
-
-  auto partition_func =
-      (mode == "k-way") ? METIS_PartGraphKway : METIS_PartGraphRecursive;
-
-  idx_t options[METIS_NOPTIONS];
-  METIS_SetDefaultOptions(options);
-  options[METIS_OPTION_ONDISK] = 1;
-  options[METIS_OPTION_NITER] = 1;
-  options[METIS_OPTION_NIPARTS] = 1;
-  options[METIS_OPTION_DROPEDGES] = 1;
-
-  if (obj_cut) {
-    options[METIS_OPTION_OBJTYPE] = METIS_OBJTYPE_CUT;
-  } else {
-    options[METIS_OPTION_OBJTYPE] = METIS_OBJTYPE_VOL;
-  }
-  CHECK(ewgt != NULL);
-  int ret = partition_func(
-      &nvtxs,  // The number of vertices
-      &ncon,   // The number of balancing constraints.
-      xadj,    // indptr
-      adjncy,  // indices
-      vwgt,    // the weights of the vertices
-      NULL,    // The size of the vertices for computing
-      // the total communication volume
       ewgt,     // The weights of the edges
       &nparts,  // The number of partitions.
       NULL,     // the desired weight for each partition and constraint
@@ -183,12 +91,12 @@ IdArray MetisPartition(
       part);
 
   if (obj_cut) {
-    LOG(INFO) << "Partition a graph with " << nvtxs << " nodes and "
-              << e_num << " edges into " << k << " parts and "
+    LOG(INFO) << "Partition a graph with " << g->NumVertices(0) << " nodes and "
+              << g->NumEdges(0) << " edges into " << k << " parts and "
               << "get " << objval << " edge cuts";
   } else {
-    LOG(INFO) << "Partition a graph with " << nvtxs << " nodes and "
-              << e_num << " edges into " << k << " parts and "
+    LOG(INFO) << "Partition a graph with " << g->NumVertices(0) << " nodes and "
+              << g->NumEdges(0) << " edges into " << k << " parts and "
               << "the communication volume is " << objval;
   }
 
@@ -226,24 +134,5 @@ DGL_REGISTER_GLOBAL("partition._CAPI_DGLMetisPartition_Hetero")
       LOG(FATAL) << "Metis partition does not support Windows.";
 #endif  // !defined(_WIN32)
     });
-
-DGL_REGISTER_GLOBAL("partition._CAPI_DGLMetisPartitionEdge_Hetero")
-    .set_body([](DGLArgs args, DGLRetValue *rv) {
-      NDArray indptr = args[0];
-      NDArray indices = args[1];
-      int k = args[2];
-      NDArray vwgt = args[3];
-      NDArray ewgt = args[4];
-      std::string mode = args[5];
-      bool obj_cut = args[6];
-      if (ewgt.NumElements() != 0) CHECK_EQ(indices.NumElements(), ewgt.NumElements());
-
-#if !defined(_WIN32)
-      *rv = MetisPartition(indptr, indices, k, vwgt, ewgt, mode, obj_cut);
-#else
-      LOG(FATAL) << "Metis partition does not support Windows.";
-#endif  // !defined(_WIN32)
-    });
-
 }  // namespace transform
 }  // namespace dgl
