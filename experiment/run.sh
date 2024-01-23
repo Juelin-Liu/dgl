21efb54a074fe14bad2f2ba2dc62a478ce195058
Juelin Liu <juelinliu@umass.edu>
2024-01-23 19:44:19 +0000

    update script

--- experiment/run.sh
+++ experiment/run.sh
@@ -5,9 +5,9 @@ MACHINE_NAME=p3_8xlarge
 
 for graph_name in orkut friendster
 do
-    for system in quiver p3 dgl
+    for model in sage gat
     do
-        for model in sage gat
+        for system in quiver p3 dgl
         do
             echo running experiment on $graph_name using $system model $model
             python run.py --system=${system} --model=${model} --nvlink=${nvlink} --graph_name=${graph_name}
