833524fe14d0b74d8b4bdabc504569346d230f20
Juelin Liu <juelin@jupiter.cs.umass.edu>
2024-04-15 18:44:43 -0400

    add download scripts

--- experiment/prepare_dataset/download.sh
+++ experiment/prepare_dataset/download.sh
@@ -12,9 +12,8 @@ pushd $data_dir
 
 aria2c -x 16 -s 16 http://snap.stanford.edu/ogb/data/nodeproppred/products.zip
 aria2c -x 16 -s 16 https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
-
-# aria2c -x 16 -s 16 http://snap.stanford.edu/ogb/data/nodeproppred/papers100M-bin.zip
-# aria2c -x 16 -s 16 https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
+aria2c -x 16 -s 16 http://snap.stanford.edu/ogb/data/nodeproppred/papers100M-bin.zip
+aria2c -x 16 -s 16 https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
 
 # wget http://snap.stanford.edu/ogb/data/nodeproppred/papers100M-bin.zip
 # wget http://snap.stanford.edu/ogb/data/nodeproppred/products.zip
@@ -23,9 +22,8 @@ aria2c -x 16 -s 16 https://snap.stanford.edu/data/bigdata/communities/com-orkut.
 
 unzip products.zip && mv products ogbn_products
 gzip -d com-orkut.ungraph.txt.gz && mkdir -p orkut && mv com-orkut.ungraph.txt orkut/orkut.txt
-
-# unzip papers100M-bin.zip && mv papers100M-bin ogbn_papers100M
-# gzip -d com-friendster.ungraph.txt.gz && mkdir -p friendster && mv com-friendster.ungraph.txt friendster/friendster.txt
+unzip papers100M-bin.zip && mv papers100M-bin ogbn_papers100M
+gzip -d com-friendster.ungraph.txt.gz && mkdir -p friendster && mv com-friendster.ungraph.txt friendster/friendster.txt
 
 rm *.zip
 rm *.gz
