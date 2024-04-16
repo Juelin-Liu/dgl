5fcf4f05e20c05aca9bd16b6f9f969a08e5add6c
Sandeep Polisetty <sandeep06011991@gmail.com>
2024-04-15 20:12:37 -0400

    added ablation code

--- experiment/ablation.sh
+++ experiment/ablation.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-for graph in products papers100M  orkut friendster;
-do
-  for model in sage gat;
+graph=papers100M;
+export PYTHONPATH=/spara/third_party/dist_cache/torch_quiver/srcs/python
+
+for model in sage gat;
+  do
+  for system in split dgl quiver;
     do
-    for system in split dgl quiver dist_cache p3;
-    	do
-	batch_size=(1024)
-	cache_sizes=(10G 8G 6G)
-	if [ $system == "dist_cache" ]; then
-           PYTHONPATH=/spara/third_party/dist_cache/quiver/srcs/python
-        fi
-	if [ $system == "quiver" ]; then
-           PYTHONPATH=/spara/third_party/quiver/quiver/srcs/python
-        fi
-	for cache_size in ${cache_sizes[@]};
-	  do
-	  export PYTHONPATH=$PYTHONPATH;echo python3 train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size}  --data_dir=/data/gsplit --cache_size=${cache_size} --batch_size=${batch_size} --log_file=main.csv
-	  unset PYTHONPATH  
-          done
-        done
+    batch_size=(1024)
+    cache_sizes=(10G 8G 6G)
+    for cache_size in ${cache_sizes[@]};   
+      do
+	  for fanout in "10,10" "10,10,10" "10,10,10,10";
+	    do
+		python3 train_main.py --system=${system} --model=${model} --fanout=${fanout} --graph=${graph} --world_size=${world_size}  --data_dir=/data/gsplit --cache_size=${cache_size} --batch_size=${batch_size} --log_file=depth.csv
+	    done 
+	  for hidden in 64 128 512;
+	    do 
+	        python3 train_main.py --system=${system} --model=${model} --fanout=15,15,15 --graph=${graph} --world_size=${world_size}  --data_dir=/data/gsplit --cache_size=${cache_size} --batch_size=${batch_size} --log_file=hidden.csv
+            done
+	  for batch_size in 512 2048;
+	    do
+                python3 train_main.py --system=${system} --model=${model} --fanout=15,15,15 --graph=${graph} --world_size=${world_size}  --data_dir=/data/gsplit --cache_size=${cache_size} --batch_size=${batch_size} --log_file=batch_size.csv
+            done
+	  python3 train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=2  --data_dir=/data/gsplit --cache_size=${cache_size} --batch_size=512  --log_file=scalability.csv
+     done
     done
 done
