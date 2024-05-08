d72ebd5ef7a8d71045bb2077fc7a927c9890989d
sandeep <sandeep>
2024-05-08 05:17:16 +0000

    fix conda environment

--- init.sh
+++ init.sh
@@ -5,9 +5,10 @@ rm -rf ${SCRIPT_DIR}/third_party/build
 
 # build GKlib
 pushd ${SCRIPT_DIR}/third_party/GKlib
-make config prefix=${SCRIPT_DIR}/third_party/build #gdb=1 debug=1
+#export GKLIB_PATH="${SCRIPT_DIR}/third_party/GKlib"
+make CONFIG_FLAGS="-D CMAKE_C_FLAGS=-D_POSIX_C_SOURCE=199309L -D CMAKE_INSTALL_PREFIX=${SCRIPT_DIR}/third_party/build" config
 make -j 
-make install 
+make install
 popd
 
 # build METIS
@@ -23,11 +24,10 @@ pushd ${SCRIPT_DIR}/third_party/oneTBB/build
 cmake -DCMAKE_BUILD_TYPE=Release -DTBB_TEST=OFF -DCMAKE_INSTALL_PREFIX=${SCRIPT_DIR}/third_party/build .. 
 cmake --build . -j 
 cmake --install . 
-popd
-
+popd 
 
 # build NCCL
 pushd ${SCRIPT_DIR}/third_party/nccl
 rm -rf ${SCRIPT_DIR}/third_party/nccl/build
-make -j src.build NVCC_GENCODE="-arch=native" BUILDDIR=${SCRIPT_DIR}/third_party/build 
+make -j src.build NVCC_GENCODE="-arch=native" BUILDDIR=${SCRIPT_DIR}/third_party/build
 popd
