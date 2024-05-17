edf7ac4128b3d40eda6013bbbc1d0ef31335a62f
sandeep06011991@gmail.com <sandeep06011991@gmail.com>
2024-05-17 19:55:24 +0000

    minor

--- experiment/script/ablation.sh
+++ experiment/script/ablation.sh
@@ -8,24 +8,27 @@ echo "Start Ablation Experiment on $graph"
 world_size=4
 export PYTHONPATH=/home/sandeep/juelin/dgl/third_party/dist_cache/torch-quiver/srcs/python
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-for model in gat; do
-  for system in quiver split; do # dgl quiver; do
+for model in sage gat; do
+  for system in split; do # split dgl quiver; do
     batch_size=(1024)
-    cache_sizes=( 2G)
+    cache_sizes=(6G 4G 2G )
+    sample_mode=gpu
     if [ $system == "dgl" ]; then
         cache_sizes=(10G)
     fi
+
+
     for cache_size in ${cache_sizes[@]}; do
-      for fanout in ; do # "10,10,10,10"  ; do #"10,10,10" "10,10,10,10"; do     
-        python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout=${fanout} --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --log_file=depth.csv
+      for fanout in  "10,10,10,10"  "10,10,10" "10,10,10,10"; do     
+         python3 ${python_dir}/train_main.py --sample_mode=${sample_mode}--system=${system} --model=${model} --fanout=${fanout} --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=1024 --log_file=depth.csv
       done
-      for hidden in 512 ; do #64 128 512; do
-        python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --hid_size=${hidden} --log_file=hidden.csv
+      for hidden in 512 64 128; do
+        python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=1024 --hid_size=${hidden} --log_file=hidden.csv
       done
-      for batch_size in 2048 ; do #512 2048; do
-        python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --log_file=batch_size.csv
+      for batch_size in 512 2048; do
+        python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --log_file=batch_size.csv
       done
-      #python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=2 --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=512 --log_file=scalability.csv
+      #python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=8 --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=2048 --log_file=scalability.csv
     done
   done
 done
--- experiment/script/main.sh
+++ experiment/script/main.sh
@@ -4,14 +4,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "${SCRIPT_DIR}/env.sh"
 echo "Start Main Benchmark Experiment"
 
-for graph in products papers100M orkut friendster; do
-  for model in sage gat; do
+for graph in  friendster; do #	products papers100M orkut friendster; do
+  for model in sage ; do #gat
     for system in split; do
 	    #dgl quiver dist_cache p3; do
       batch_size=(1024)
       cache_sizes=(10G)
       if [ $system == "split" ]; then
           cache_sizes=(10G 8G 6G 4G)
+      	  cache_sizes=(6G)
       fi
       if [ $system == "dist_cache" ]; then
         PYTHONPATH=/spara/third_party/dist_cache/torch-quiver/srcs/python
