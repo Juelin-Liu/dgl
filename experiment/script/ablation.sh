6f10b3a59d0c348a65e3e4c28cb3846ff3313c2b
sandeep06011991@gmail.com <sandeep06011991@gmail.com>
2024-06-21 02:18:45 +0000

    minor changes

--- experiment/script/ablation.sh
+++ experiment/script/ablation.sh
@@ -2,35 +2,48 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "${SCRIPT_DIR}/env.sh"
-graph=papers100M
+graph=friendster
 echo "Start Ablation Experiment on $graph"
 
 world_size=4
 export PYTHONPATH=/home/sandeep/juelin/dgl/third_party/dist_cache/torch-quiver/srcs/python
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-for model in sage gat; do
-  for system in quiver; do # split dgl quiver; do
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+for model in  gat ; do
+
+  for system in split; do
     batch_size=(1024)
     cache_sizes=(8G)
     sample_mode=uva
     if [ $system == "dgl" ]; then
         cache_sizes=(10G)
     fi
-
+    if [ $model == "sage" ]; then
+	cache_sizes=(6G 4G)
+	if [ $system == "split" ]; then 
+		sample_mode=uva
+	fi 
+    fi 
+    if [ $model == "gat" ]; then
+        cache_sizes=(0G)
+    fi
+    
+	    
 
     for cache_size in ${cache_sizes[@]}; do
-      for fanout in  "10,10,10,10" ;do # "10,10,10" ; do 
+      for fanout in  "10,10,10,10"  ; do 
 	batch_size=(1024)    
-        python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout=${fanout} --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=1024 --log_file=depth.csv
+        python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout=${fanout} --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=1024 --log_file=depth.csv --hid_size=256
       done
-      for hidden in 512 64 128; do
+      for hidden in 384; do
 	batch_size=(1024)
         echo python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=1024 --hid_size=${hidden} --log_file=hidden.csv
       done
-      for batch_size in 512 2048; do
-        echo python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --log_file=batch_size.csv
+      for batch_size in 2048; do
+        echo python3 ${python_dir}/train_main.py --sample_mode=${sample_mode} --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=${world_size} --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=${batch_size} --log_file=batch_size.csv --hid_size=256
       done
-      echo python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=2 --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=512 --log_file=scalability.csv
+      echo python3 ${python_dir}/train_main.py --system=${system} --model=${model} --fanout="15,15,15" --graph=${graph} --world_size=8 --data_dir=${data_dir} --cache_size=${cache_size} --batch_size=2048 --log_file=scalability.csv
     done
   done
 done
+echo "All ablation done"
--- experiment/script/distributed.sh
+++ experiment/script/distributed.sh
@@ -18,13 +18,13 @@ export PYTHONPATH=/home/sandeep/juelin/dgl/third_party/dist_cache/torch-quiver/s
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
-for system in dgl;
+for system in quiver;
 
 do
 for model in  sage gat;
 do
 	python3  ${python_dir}/train_main.py  --system=${system} --model=${model} --fanout=15,15,15 --graph=${graph} --world_size=4 \
-	--data_dir=${data_dir}  --cache_size=6G --batch_size=4096 --num_epoch=3  --num-nodes=4 --node-rank=0 --log_file=distributed.csv
+	--data_dir=${data_dir}  --cache_size=6G --batch_size=1024 --num_epoch=3  --num-nodes=2 --node-rank=0 --log_file=distributed.csv
 
 
 done
